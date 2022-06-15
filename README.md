37c36d91482b36ba64ddb010a0e5e3c9269c57f9
Matt <1009003+tantaman@users.noreply.github.com>
2022-06-15 16:58:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Aphrodite Node Starter
+
+This provides a basic NodeJS example project that uses `Aphrodite`.
+
+- The schema is located in `src/domain.aphro`
+- A simple application is in `src/main.ts`
+
+# Getting Started
+
+First, clone this repository
+
+```bash
+git clone git@github.com:tantaman/aphrodite-node-starter.git
+```
+
+Next, cd to `aphrodite-node-starter` and install dependencies.
+
+```bash
+cd aphrodite-node-starter
+npm install
+```
+
+The commands to build and run are the "demo app" are:
+
+```bash
+npm run build
+npm run main
+```
+
+For more info, see the `Aphrodite` [quickstart guide](https://aphrodite.sh/docs/quickstart)
---
+++ src/main.ts
@@ -0,0 +1,93 @@
+import {
+  context,
+  anonymous,
+  basicResolver,
+  sql,
+  commit,
+  Context,
+  P,
+} from "@aphro/runtime-ts";
+import connect, { DatabaseConnection } from "@databases/sqlite";
+import TodoListMutations from "./generated/TodoListMutations.js";
+import TodoMutations from "./generated/TodoMutations.js";
+import { fileURLToPath } from "url";
+import path from "path";
+import { readdirSync } from "fs";
+import TodoList from "./generated/TodoList.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function main() {
+  const db = connect("test.db");
+  await createTables(db);
+  const ctx = context(anonymous(), basicResolver(db));
+
+  await createTestData(ctx);
+  await runSomeQueries(ctx);
+}
+
+async function createTestData(ctx: Context) {
+  let lists = await TodoList.queryAll(ctx).gen();
+
+  if (lists.length !== 0) {
+    return;
+  }
+
+  const listCs = TodoListMutations.create(ctx, {
+    name: "Bucket List",
+  }).toChangeset();
+
+  // Create all items in the same write.
+  const [storageHandle] = commit(
+    ctx,
+    listCs,
+    TodoMutations.create(ctx, {
+      list: listCs,
+      text: "Read",
+    }).toChangeset(),
+    TodoMutations.create(ctx, {
+      list: listCs,
+      text: "Write",
+    }).toChangeset(),
+    TodoMutations.create(ctx, {
+      list: listCs,
+      text: "Code",
+    }).toChangeset(),
+    TodoMutations.create(ctx, {
+      list: listCs,
+      text: "Sleep",
+    }).toChangeset(),
+    TodoMutations.create(ctx, {
+      list: listCs,
+      text: "Eat",
+    }).toChangeset(),
+    TodoMutations.create(ctx, {
+      list: listCs,
+      text: "Drink",
+    }).toChangeset()
+  );
+
+  await storageHandle;
+}
+
+async function runSomeQueries(ctx: Context) {
+  const todos = await TodoList.queryAll(ctx)
+    .whereName(P.equals("Bucket List"))
+    .queryTodos()
+    .gen();
+  console.log(todos.map((t) => t.text));
+}
+
+async function createTables(db: DatabaseConnection) {
+  const generatedDir = path.join(__dirname, "..", "src", "generated");
+  const schemaPaths = readdirSync(generatedDir).filter((name) =>
+    name.endsWith(".sqlite.sql")
+  );
+
+  const schemas = schemaPaths.map((s) => sql.file(path.join(generatedDir, s)));
+
+  await Promise.all(schemas.map((s) => db.query(s)));
+}
+
+await main();
